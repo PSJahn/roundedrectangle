5827cffe0d22d4c562e1179426f5ac0a3940cfae
PSJahn <66626848+PSJahn@users.noreply.github.com>
2025-02-01 19:39:10 +0100

    Removed test rendering

--- src/main/java/de/psjahn/rr/RoundedRectangleMod.java
+++ src/main/java/de/psjahn/rr/RoundedRectangleMod.java
@@ -1,11 +1,7 @@
 package de.psjahn.rr;
 
-import de.psjahn.rr.mixinHelper.RoundedRectangleDrawContext;
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
 import net.minecraft.util.Identifier;
-
-import java.awt.*;
 
 public class RoundedRectangleMod implements ClientModInitializer {
 	public static final String MOD_ID = "rounded-rectangle";
@@ -16,10 +12,6 @@ public class RoundedRectangleMod implements ClientModInitializer {
 
 	@Override
 	public void onInitializeClient() {
-		HudRenderCallback.EVENT.register(((drawContext, renderTickCounter) -> {
-			RoundedRectangleDrawContext roundedContext = (RoundedRectangleDrawContext) drawContext;
-			drawContext.fill(0, 0, 50, 50, 10, Color.WHITE.getRGB());
-			roundedContext.fillRounded(50, 0, 150, 50, 10, 20, Color.RED.getRGB());
-		}));
+
 	}
 }
